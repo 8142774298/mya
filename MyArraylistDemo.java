99f91310107d1f9a8084ccd7d310fc20c98d6594
8142774298 <72378411+8142774298@users.noreply.github.com>
2022-08-22 11:57:58 +0530

    Create MyArraylistDemo.java

---
+++ MyArraylistDemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class MyArraylistDemo {
+	public static void main(String[] args) {
+		ArrayList<Integer> ar=new ArrayList<>();
+		ar.add(2);
+		ar.add(3);
+		ar.add(1);
+		ar.add(4);
+		ar.add(5);
+		System.out.println(ar);
+	}
+}
+//executed onprogrammiz online java compiler
